7d4b88dc8deb02ca516e0d1c6a5913d9fccb3f11
Kei-K23 <keiksl2301@gmail.com>
2024-10-08 14:24:01 +0630

    Add star container component

--- src/components/stars/star-container.tsx
+++ src/components/stars/star-container.tsx
@@ -1,3 +1,18 @@
-export default function StarContainer() {
-  return <div></div>
+import { JSX } from "solid-js";
+
+interface StarContainerProps {
+  children: JSX.Element;
+}
+export default function StarContainer({ children }: StarContainerProps) {
+  return (
+    <div
+      style={{
+        display: "inline-flex",
+        "align-items": "center",
+        cursor: "pointer",
+      }}
+    >
+      {children}
+    </div>
+  );
 }
