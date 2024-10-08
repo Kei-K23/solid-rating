2e0e3a1abd78a763d408455a6b16282bd03490db
Kei-K23 <keiksl2301@gmail.com>
2024-10-08 14:09:51 +0630

    Setup interface for Star component

--- src/components/stars/star.tsx
+++ src/components/stars/star.tsx
@@ -1,3 +1,16 @@
-export default function Star() {
+interface StarProps {
+  filled: boolean
+  halfFilled: boolean
+  onMouseMove: (event: MouseEvent) => void
+  onMouseLeave: () => void
+  onClick: () => void
+  size?: number
+  color?: string
+  readOnly?: boolean
+  activeColor?: string
+  customIcon?: string
+}
+
+export default function Star(props: StarProps) {
   return <div></div>
 }
